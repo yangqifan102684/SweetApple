feb380913fae0f5401e1ea88b17147b7bbd81ab1
yangqifan004 <yangqifan004@ke.com>
2021-12-14 20:24:39 +0800

    login logout

--- src/main/java/com/sweet/apple/sweetapple/controller/TestController.java
+++ src/main/java/com/sweet/apple/sweetapple/controller/TestController.java
@@ -1,8 +1,12 @@
 package com.sweet.apple.sweetapple.controller;
 
+import cn.dev33.satoken.stp.StpLogic;
+import cn.dev33.satoken.stp.StpUtil;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * 描述
@@ -13,12 +17,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @Value("${test.config.hei}")
-    String value;
-
     @RequestMapping("/test")
-    public String test(String input){
+    public String test(HttpServletRequest request){
 
-        return "config:" + value + "result:" + input;
+        return "success";
     }
+
+    @RequestMapping("/login")
+    public String login(String username,String password, HttpServletRequest request){
+        StpUtil.login(username);
+        StpUtil.getSession().set("key","value");
+        return "success";
+    }
+
+    @RequestMapping("/logout")
+    public String logout(HttpServletRequest request){
+        StpUtil.logout();
+        return "success";
+    }
+
+    @RequestMapping("/get")
+    public String t(){
+        Object value = StpUtil.getSession().get("key");
+        System.out.println(value);
+        return "success";
+    }
+
+
 }
